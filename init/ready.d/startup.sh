d8320865979fb11dc8c018c66e4619a59b141f03
tomoya sunda <dexn.dev@outlook.jp>
2024-04-04 20:16:59 +0900

    (#123) CORS を設定する

--- init/ready.d/startup.sh
+++ init/ready.d/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 awslocal s3 mb s3://mybucket
-cd /home/localstack/data/
-awslocal s3 cp myinitialdata.txt s3://mybucket/
+awslocal s3api put-bucket-cors --bucket mybucket --cors-configuration file:///etc/localstack/init/ready.d/cors.json
+awslocal s3 cp /home/localstack/data/myinitialdata.txt s3://mybucket/
