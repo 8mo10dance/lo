d2509f6712171a272376615d0da7024bf6f70198
tomoya sunda <dexn.dev@outlook.jp>
2024-04-04 20:16:59 +0900

    (#123) feat: マスターデータを変更する

--- init/ready.d/startup.sh
+++ init/ready.d/startup.sh
@@ -2,4 +2,7 @@
 
 awslocal s3 mb s3://mybucket
 awslocal s3api put-bucket-cors --bucket mybucket --cors-configuration file:///etc/localstack/init/ready.d/cors.json
-awslocal s3 cp /home/localstack/data/myinitialdata.txt s3://mybucket/
+for f in /home/localstack/data/*.json
+do
+  awslocal s3 cp $f s3://mybucket/
+done
