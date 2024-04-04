1d1630c3bd24175aad3d2616487661b2cc28fa34
tomoya sunda <dexn.dev@outlook.jp>
2024-04-04 20:16:59 +0900

    (#123) chore: コードを整理する

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-
-const f = (x: number) => x + 1
+import App from './App'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app')
@@ -10,7 +9,3 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = createRoot(app)
   root.render(<App />)
 })
-
-function App() {
-  return <div>{`output: ${f(10)}`}</div>
-}
