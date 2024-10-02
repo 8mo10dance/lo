15ded3aa3ac978d2c63d593ed7d8b31adff821a6
sunda <8mo10dance@protonmail.com>
2024-10-02 17:52:20 +0900

    (#268) feat: ユーザー登録フォームの見た目を実装する

--- client/src/pages/users/New.tsx
+++ client/src/pages/users/New.tsx
@@ -3,5 +3,25 @@ import React from "react";
 import Layout from "../../components/Layout";
 
 export default function New() {
-  return <Layout title="ユーザー登録">TODO</Layout>;
+  return (
+    <Layout title="ユーザー登録">
+      <form>
+        <div>
+          <label>
+            Name:
+            <input name="user.name" type="text" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Email:
+            <input name="user.email" type="email" />
+          </label>
+        </div>
+        <div>
+          <input type="submit" value="登録" />
+        </div>
+      </form>
+    </Layout>
+  );
 }
