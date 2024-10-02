d40a8f951e3fb5602ac0f800d7ed40c124a35dde
sunda <8mo10dance@protonmail.com>
2024-10-02 18:07:52 +0900

    (#268) feat: zod でバリデーションをかける

--- client/src/pages/users/New.tsx
+++ client/src/pages/users/New.tsx
@@ -1,25 +1,44 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
 import Layout from "../../components/Layout";
 
+const schema = z.object({
+  user: z.object({
+    name: z.string().min(1),
+    email: z.string().min(1).email(),
+  }),
+});
+
+type UserFormType = z.infer<typeof schema>;
+
 export default function New() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserFormType>({
+    resolver: zodResolver(schema),
+  });
 
   return (
     <Layout title="ユーザー登録">
-      <form onSubmit={handleSubmit((values) => console.log(values))}>
+      <form noValidate onSubmit={handleSubmit((values) => console.log(values))}>
         <div>
           <label>
             Name:
             <input type="text" {...register("user.name")} />
           </label>
+          {errors.user?.name && <p>{errors.user?.name?.message}</p>}
         </div>
         <div>
           <label>
             Email:
             <input type="email" {...register("user.email")} />
           </label>
+          {errors.user?.email && <p>{errors.user?.email?.message}</p>}
         </div>
         <div>
           <input type="submit" value="登録" />
