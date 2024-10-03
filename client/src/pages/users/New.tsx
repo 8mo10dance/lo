bdafa69f78faccf6c730ff29b70d24fc617384eb
sunda <8mo10dance@protonmail.com>
2024-10-03 18:44:44 +0900

    (#268) fix: バリデーションが間違っているのを修正する

--- client/src/pages/users/New.tsx
+++ client/src/pages/users/New.tsx
@@ -24,7 +24,7 @@ const schema = z.object({
       .refine(async (value) => {
         const isDuplicated = await duplicatedEmail(value);
 
-        return isDuplicated;
+        return !isDuplicated;
       }, "Email is already used"),
   }),
 });
