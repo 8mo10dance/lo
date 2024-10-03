070e5efa8972d0b2e86907085a936d17c4903ee1
sunda <8mo10dance@protonmail.com>
2024-10-03 19:12:34 +0900

    Migrate DB

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_29_030241) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_03_100539) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
