f67faa90f9e8a792c3d4c8eab1e5eeb10ef3e9a9
sunda <8mo10dance@protonmail.com>
2024-11-03 16:40:42 +0900

    wip

--- client/src/api/users.ts
+++ client/src/api/users.ts
@@ -4,5 +4,5 @@ import { User } from '../types'
 export async function getUsers(): Promise<User[]> {
   const data = await apiRequest<{ users: User[] }>('/api/v1/users')
 
-  return data.users
+    return data.users
 }
